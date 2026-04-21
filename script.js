06c58433b24cbd214a45e4ab15d9283fe70606e2
RBence12 <renk.bence2009@tanulo.boronkay.hu>
2026-04-21 10:33:12 +0200

    Kvíz Menü 90%

--- script.js
+++ script.js
@@ -45,19 +45,61 @@ function kvizErtekel() {
 
 
 function kvizErtekeltext(){
+
     var pontszamtext = 0;
      
-    var valasztext1 = document.getElementById("kerdestext1").value;
+    // Válaszok tartalmának kinyerése
+
+    var Valasztext1 = document.getElementById("kerdestext1").value;
+    var Valasztext2 = document.getElementById("kerdestext2").value;
+    var Valasztext3 = document.getElementById("kerdestext3").value;
+    var Valasztext4 = document.getElementById("kerdestext4").value;
 
 
 // Kis és Nagybetű elleni "védekezés"
-        if("Valasztext1.toLowerCase() === "Helyes válasz helye || Valasztext1.toLowerCase() === "Ha több helye válasz akkor azt ide" ){
-            pontszamtext++;
+if (Valasztext1.toLowerCase() === "ferrari" || Valasztext1.toLowerCase() === "vörös") {
+    pontszamtext++;
+}
+if (Valasztext2.toLowerCase() === "ferrari" || Valasztext2.toLowerCase() === "vörös") {
+    pontszamtext++;
+}
+if (Valasztext3.toLowerCase() === "ferrari" || Valasztext3.toLowerCase() === "vörös") {
+    pontszamtext++;
+}
+if (Valasztext4.toLowerCase() === "ferrari" || Valasztext4.toLowerCase() === "vörös") {
+    pontszamtext++;
+}
 
-        } 
+        //Végeredmény üzenet 
+
+        if(pontszamtext === 4){
+            szovegesErtekelestext = "Hibáltan! Gratulálok, tökéletesen teljesítetted a tesztet."
+        } else if (pontszamtext >= 2) { 
+            szovegesErtekelestext = "Szépen teljesítettél a teszten, de azért volt benne 1-2 két hiba :)."
+
+        } else{
+            szovegesErtekelestext = "Ez most olyan lett, mint az 1959-es szezon... Olvasd át újra a szöveget! 🔧";
+        }
+
+
+         // Ellenőrzés: mindenre érkezett-e válasz?
+        if (!Valasz1 || !Valasz2 || !Valasz3 || !Valasz4) {
+        const hibaDoboztext = document.getElementById("eredmeny-doboztext");
+        hibaDoboztext.innerHTML = "Légy szíves, jelölj be egy választ minden kérdésnél!";
+        hibaDoboztext.style.color = "orange";
+        return; 
+    }
+
+    
+    // Szöveges visszajelzés az eredményről
+
+    document.getElementById("szoveges_visszajelzes").innerHTML =
 
     // Eredmény kiiratása
     
-    document.getElementById("eredmenytext")innerHTML = "Pontszámod:" + pontszam "/4";
+    document.getElementById("eredmeny_helyetext").innerHTML = "Pontszámod:" + pontszamtext + "/4";
+}
+
+
 
     
